e61895c59e0f8fd2d08923f5afd372df166bb260
Bart Guijt <bart@guijt.me>
2024-01-10 23:04:11 +0100

    Added new, add, find and all methods to 'Greetings'. Finished 4.1

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,29 +1,62 @@
 #[derive(Debug)]
+#[derive(PartialEq)]
 enum Lang {
   English,
   Spanish,
   Chinese,
   Texan,
+  Dutch,
 }
 
+#[derive(Debug)]
 struct Greeting {
     message: String,
     lang: Lang,
 }
 
+struct Greetings {
+  greetings: Vec<Greeting>,
+}
+
+impl Greetings {
+  fn new() -> Greetings {
+    Greetings {
+      greetings: Vec::new()
+    }
+  }
+
+  fn add(self: &mut Greetings, greeting: Greeting) {
+    self.greetings.push(greeting);
+  }
+
+  fn find(self: &Greetings, lang: Lang) -> Result<&Greeting, String> {
+    for g in &self.greetings {
+      if g.lang == lang {
+        return Ok(g);
+      }
+    }
+    Err(String::from("Not found"))
+  }
+
+  fn all(self: &Greetings) -> &Vec<Greeting> {
+    &self.greetings
+  }
+}
+
 fn main() {
-  let mut v :Vec<Greeting> = Vec::new();
+  let mut v: Greetings = Greetings::new();
 
-  let g : Greeting = Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
-  v.push(g);
-  let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
-  v.push(g);
+  v.add(Greeting { lang: Lang::English, message: String::from("Hello WasmEdge!") });
+  v.add(Greeting { lang: Lang::Spanish, message: String::from("Hola WasmEdge!") });
+  v.add(Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") });
+  v.add(Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") });
+  v.add(Greeting { lang: Lang::Dutch, message: String::from("Hallo WasmEdge!") });
 
-  for e in v {
+  for e in v.all() {
     println!("{:?} {}", e.lang, e.message);
   }
+
+  println!("Spanish: {:?}", v.find(Lang::Spanish).unwrap());
+  println!("Spanish: {:?}", v.find(Lang::Spanish).unwrap());
+  println!("Texan: {:?}", v.find(Lang::Texan).unwrap());
 }
